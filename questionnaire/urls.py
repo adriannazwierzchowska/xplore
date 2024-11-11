3ed2bfb1c292fcc17d97e45299d4b5936dd47bd2
Klaudia <jozwikk1@pw.edu.pl>
2024-11-11 21:26:57 +0100

    zmiany

--- questionnaire/urls.py
+++ questionnaire/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserResponseCreateView
+from .views import UserResponseCreateView, UserResponseListView
 
 urlpatterns = [
     path('responses/', UserResponseCreateView.as_view(), name='submit-response'),
+    path('responses/me/', UserResponseListView.as_view(), name='user-responses'),
 ]
