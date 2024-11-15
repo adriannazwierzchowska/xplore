545739fe96fe6d70e7b20558e4d759bdad7d79a2
Adrianna Zwierzchowska <01178899@pw.edu.pl>
2024-11-15 16:29:36 +0100

    Added working frontend for login and register.

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import Home from './Home';
+import Login from './Login';
+import Register from './Register';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+      <Router>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+          </Routes>
+      </Router>
+  );
+}
+
+export default App;
