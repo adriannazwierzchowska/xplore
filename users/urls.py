448f7912be044316b1babdef93573289bbf46e67
Adrianna Zwierzchowska <01178899@pw.edu.pl>
2024-11-15 16:29:36 +0100

    Added working frontend for login and register.

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
     path('login/', views.login_user, name='login_user'),
     path('register/', views.register_user, name='register_user'),
     path('logout/', views.logout_user, name='logout_user'),
+    #path('current_user/', views.current_user, name='current_user'),
 ]
