020d844b8a203f6f4a764168f8c1919c73a5308a
Maciej Przybylski <42326412+cyvn7@users.noreply.github.com>
2024-11-20 01:04:06 +0100

    Added all working screens of a questionnaire

---
+++ frontend/src/views/FormCuisine.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { CiForkAndKnife } from "react-icons/ci";
+import '../front.css';
+
+const FormCuisine = () => {
+    const navigate = useNavigate();
+    const [cuisine, setCuisineRange] = useState(0);
+
+    const addCuisine = async (e) => {
+        e.preventDefault();
+        sessionStorage.setItem('cuisine', cuisine);
+        navigate('/analise');
+    };
+
+    return (
+        <div className="form">
+            <form>
+            <h1>What kind of weather do you prefer?</h1>
+            <div className="slidecontainer">
+                <div className="slider-labels">
+                    <span style={{ float: 'left', marginBottom: '10px' }}>Not important</span>
+                    <span style={{ float: 'right', marginBottom: '10px' }}>Very important</span>
+                </div>
+                <input 
+                    type="range" 
+                    min="1" 
+                    max="5" 
+                    value={cuisine} 
+                    className="slider" 
+                    id="weatherRange" 
+                    onChange={(e) => setCuisineRange(e.target.value)} 
+                />
+            </div>
+            </form>
+            <div className="bottom-button-group">
+                <button type="button3" onClick={() => navigate(-1)}>
+                    <span className="chevron-left"></span> Go Back
+                </button>
+                <div className="right-buttons">
+                    <button type="button3" onClick={() => navigate('/analise')}>
+                        Skip
+                    </button>
+                    <button type="button1" onClick={addCuisine}>
+                        Next <span className="chevron-right"></span>
+                    </button>
+                </div>
+            </div>
+            <div className="page-icon">
+                <CiForkAndKnife size={200} />
+            </div>
+        </div>
+    );
+};
+
+export default FormCuisine;
---
+++ frontend/src/views/Home.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { currentUser, logoutUser } from "../api";
+import '../front.css';
+
+const Home = () => {
+    const navigate = useNavigate();
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [username, setUsername] = useState(localStorage.getItem('authToken') || null);
+    const [message, setMessage] = useState('');
+
+    // useEffect(() => {
+    //     try {
+    //         const response = currentUser();
+    //         setUsername(response.data.username);
+    //         setIsAuthenticated(true);
+    //     } catch (error) {
+    //         setMessage(error.response?.data?.error || '"Error fetching user info"');
+    //     }
+    // }, []);
+
+    useEffect(() => {
+        const token = localStorage.getItem('authToken');
+        setIsAuthenticated(!!token);
+    }, []);
+
+    const handleLetsGo = () => {
+        if (isAuthenticated) {
+            navigate('/questionnaire_1');
+        } else {
+            navigate('/login');
+        }
+    };
+
+    const handleLogout = async () => {
+        try {
+            const response = await logoutUser();
+            localStorage.removeItem('authToken');
+            setUsername(null);
+            navigate('/login');
+        } catch (error) {
+            setMessage(error.response?.data?.error || '"Logout error"');
+        }
+    };
+
+    return (
+        <div>
+            <form>
+                <h1>Let's xplore</h1>
+                    <div className="button-group">
+                    <button type="button1" onClick={handleLetsGo}>Let's Go!</button>
+                    <button type="button2">Favorites</button>
+                    {isAuthenticated && <button type="button2" onClick={handleLogout}>Log Out</button>}
+                    {username ? <p>Hi, {username}</p> : null}
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default Home;
