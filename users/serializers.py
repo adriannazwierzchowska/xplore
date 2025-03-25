a5404a3704ebb1ee8331ffba69e3f362067ec0c2
Klaudia <klaudia.jozwik57@gmail.com>
2025-03-25 19:57:41 +0100

    added like counter

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import UserFavorite
 
+
 class UserFavoriteSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserFavorite
         fields = ['place']
+
