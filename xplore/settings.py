b8e19969400c4d35fed5ac4ad8de4fb3f4648972
Adrianna Zwierzchowska <01178899@pw.edu.pl>
2025-04-28 11:20:23 +0200

    Database server integration

--- xplore/settings.py
+++ xplore/settings.py
@@ -72,11 +72,11 @@ WSGI_APPLICATION = 'xplore.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'xplore',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
