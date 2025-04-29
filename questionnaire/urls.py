5715fc1217ec5f1cbfa41874afb5da9dd39d44da
Adrianna Zwierzchowska <01178899@pw.edu.pl>
2025-04-29 12:59:46 +0200

    Favorites page view

--- questionnaire/urls.py
+++ questionnaire/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import classify
+from .views import classify, get_place_coordinates
 
 urlpatterns = [
     path('classify/', classify, name='classify'),
+    path('place_coordinates/', get_place_coordinates, name='place_coordinates'),
 ]
