ebce187f95bbc35361739be01322422d782db85d
Adrianna Zwierzchowska <01178899@pw.edu.pl>
2025-05-02 13:44:27 +0200

    Changed star display, added tags to favorite places, fixed google API connection

--- xplore/settings.py
+++ xplore/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -40,13 +41,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 CORS_ALLOW_CREDENTIALS = True
+
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
 ]
-
 
 ROOT_URLCONF = 'xplore.urls'
 
@@ -77,6 +77,7 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
+        'POOL_MODE': os.getenv('POOL_MODE'),
     }
 }
 
