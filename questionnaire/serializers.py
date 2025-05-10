7b76b7051778166cb9ac4e55e0ff5c40166b6eb5
Maciej Przybylski <42326412+cyvn7@users.noreply.github.com>
2025-05-10 23:24:03 +0200

    -page title is "xplore" now -moved mute button to the sidebar

--- questionnaire/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import UserResponse
-
-class UserResponseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserResponse
-        fields = '__all__'
-        read_only_fields = ('user',)
